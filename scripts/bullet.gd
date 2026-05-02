5ca71c2c61f52d54913560411cba29c23a653cde
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2026-05-01 20:25:37 -0400

    proper bullets

---
+++ scripts/bullet.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+var speed = 6
+
+func _process(delta):
+	var forward_vector = -global_transform.basis.z 
+	global_position += forward_vector * speed * delta
--- scripts/shootingManager.gd
+++ scripts/shootingManager.gd
@@ -1,9 +1,11 @@
 extends Node
 
 @export var shootPos : Node3D
+@export var cooldownTime = 0.2
 
 @onready var cooldown = Timer.new()
 @onready var baseBullet = preload("res://scenes/bullet.tscn")
+
 
 
 var canFire = true
@@ -12,7 +14,7 @@ var bullets = []
 
 func _ready():
 	add_child(cooldown)
-	cooldown.wait_time = 0.2
+	cooldown.wait_time = cooldownTime
 	cooldown.one_shot = true
 	cooldown.timeout.connect(_resetCooldown)
 
@@ -28,7 +30,7 @@ func shoot():
 	var newBullet : Node3D = baseBullet.instantiate()
 	get_parent().add_sibling(newBullet)
 	newBullet.global_position = shootPos.global_position
-	
+	newBullet.global_rotation = shootPos.global_rotation
 
 func _resetCooldown():
 	canFire = true
