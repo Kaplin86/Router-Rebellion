d60c14af918dc56d232fda724968e37ca91a2e47
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2026-05-15 14:42:25 -0400

    load model wip

--- scripts/dialogueUI.gd
+++ scripts/dialogueUI.gd
@@ -10,6 +10,9 @@ extends Control
 @export var leftCharNode : Sprite2D
 @export var rightCharNode : Sprite2D
 
+var currentLeftModel : String = ""
+var currentRightModel : String = ""
+
 signal continuPressed
 
 var time := 0.0
@@ -45,9 +48,31 @@ func bounce(node):
 	bounceTarget = node
 
 func doDialogue(dialogue : dialogueChunk):
-	if dialogue.leftCharName != leftCharacter:
-		# set the model
-		pass
+	if dialogue.leftCharModel != "":
+		if dialogue.leftCharModel != currentLeftModel:
+			currentLeftModel = dialogue.leftCharModel
+			
+			var children =  leftCharNode.get_child(0).get_children()
+			for I in children:
+				if I.is_inside_tree():
+					I.free()
+				
+			var model = load(dialogue.leftCharModel).instantiate()
+			leftCharNode.get_child(0).add_child(model)
+			
+			
+			var camera = Camera3D.new()
+			leftCharNode.get_child(0).add_child(camera)
+			camera.global_position = Vector3(0.381,0.457,-2.197)
+			camera.global_rotation_degrees = Vector3(-2.9,168.6,0.0)
+			print(leftCharNode.get_child(0).get_children())
+	else:
+		currentLeftModel = ""
+		var children =  leftCharNode.get_child(0).get_children()
+		for I in children:
+			if I.is_inside_tree():
+				I.free()
+		
 	if dialogue.rightCharName != rightCharacter:
 		# set the model
 		pass
--- tools/dialogueHelper/dialogue_helper_scene.gd
+++ tools/dialogueHelper/dialogue_helper_scene.gd
@@ -14,6 +14,10 @@ var buttonScene = preload("res://tools/dialogueHelper/dialogueMinibutton.tscn")
 @export var leftName : LineEdit
 @export var rightName : LineEdit
 @export var speakingSide : CheckButton
+@export var leftModelButton : Button
+@export var rightModelButton : Button
+@export var leftModelAnims : OptionButton
+@export var rightModelAnims : OptionButton
 
 func _ready():
 	current = DialogueHolder.new()
@@ -45,31 +49,37 @@ func loadDialogueButtons():
 
 
 func _on_button_pressed():
+	path = createDialogueFromPath(await fileAsk(["*.tres"]))
+	loadDialogueButtons()
+
+func fileAsk(filter):
 	var file_dialog = FileDialog.new()
-	file_dialog.filters = PackedStringArray(["*.tres"])
+	file_dialog.filters = PackedStringArray(filter)
 	EditorInterface.get_base_control().add_child(file_dialog)
 	file_dialog.popup_centered_ratio()
 	var temp = await file_dialog.file_selected
-	var resource = null
-	if FileAccess.file_exists(temp):
-		resource = load(temp)
-		if not resource is DialogueHolder:
-			resource = null
-			current = DialogueHolder.new()
-		else:
-			
-			current = DialogueHolder.new()
-			for I in resource.dialogue:
-				current.dialogue.append(I)
-	else:
-		resource = DialogueHolder.new()
 	
-	path = temp
+	
+	
 	await get_tree().process_frame
 	var wind :Window = get_parent()
 	wind.grab_focus()
-	loadDialogueButtons()
+	return temp
 
+
+func createDialogueFromPath(wowpath):
+	var resource = null
+	if FileAccess.file_exists(wowpath):
+		var loaded = load(wowpath)
+		if not loaded is DialogueHolder:
+			resource = DialogueHolder.new()
+		else:
+			resource = DialogueHolder.new()
+			for I in loaded.dialogue:
+				resource.dialogue.append(I)
+	else:
+		resource = DialogueHolder.new()
+	return resource
 
 func _on_add_empty_pressed():
 	var newDIALOGUE = dialogueChunk.new()
@@ -100,8 +110,32 @@ func displayChunk(chunk : dialogueChunk):
 	leftName.text = currentChunk.leftCharName
 	rightName.text = currentChunk.rightCharName
 	speakingSide.button_pressed = bool(currentChunk.speakingChar)
+	leftModelButton.text = currentChunk.leftCharModel
+	rightModelButton.text = currentChunk.rightCharModel
+	
+	loadChoiceButton(leftModelAnims,getAnimsFromModel(currentChunk.leftCharModel),currentChunk.leftAnim)
+	loadChoiceButton(rightModelAnims,getAnimsFromModel(currentChunk.rightCharModel),currentChunk.rightAnim)
 	
 	updateDisplay()
+
+func getAnimsFromModel(modelpath):
+	if !FileAccess.file_exists(modelpath):
+		return []
+	var packed : PackedScene = load(modelpath)
+	var model : Node = packed.instantiate()
+	var animationPlayer : AnimationPlayer= model.find_children("*","AnimationPlayer")[0]
+	var anims = animationPlayer.get_animation_list()
+	return Array(anims)
+
+func loadChoiceButton(node : OptionButton,options : Array, choice = null):
+	node.clear()
+	for I in options:
+		node.add_item(I)
+	
+	if choice != null:
+		if options.find(choice):
+			node.selected = options.find(choice)
+		
 
 func updateDisplay():
 	dialoguePlayer.doDialogue(currentChunk)
@@ -112,6 +146,29 @@ func on_any_change(param = null, param2 = null, param3 = null):
 	currentChunk.leftCharName = leftName.text
 	currentChunk.rightCharName = rightName.text
 	currentChunk.speakingChar = int(speakingSide.button_pressed)
+	currentChunk.leftCharModel = leftModelButton.text
+	currentChunk.rightCharModel = rightModelButton.text
+	
+	var selectedIdLeft = leftModelAnims.get_selected_id()
+	var selectedIdRight = rightModelAnims.get_selected_id()
+	
+	loadChoiceButton(leftModelAnims,getAnimsFromModel(currentChunk.leftCharModel))
+	loadChoiceButton(rightModelAnims,getAnimsFromModel(currentChunk.rightCharModel))
+	
+	if leftModelAnims.item_count != 0:
+		currentChunk.leftAnim = leftModelAnims.get_item_text(selectedIdLeft)
+		print("anim is set to",currentChunk.leftAnim )
+	if rightModelAnims.item_count != 0:
+		currentChunk.rightAnim = rightModelAnims.get_item_text(selectedIdRight)
+	
+	loadChoiceButton(leftModelAnims,getAnimsFromModel(currentChunk.leftCharModel),currentChunk.leftAnim)
+	loadChoiceButton(rightModelAnims,getAnimsFromModel(currentChunk.rightCharModel),currentChunk.rightAnim)
 	
 	updateDisplay()
 	loadDialogueButtons()
+
+
+func _on_set_l_model_pressed():
+	var modelpath = await fileAsk(["*.tscn"])
+	leftModelButton.text = modelpath
+	on_any_change()
